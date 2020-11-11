baba4a5fc40a94fcd15e7543cd79e05b7ac0180c
leongus1 <leongus1@gmail.com>
2020-11-10 21:23:13 -0800

    git ignore

---
+++ pokemon_project/secrec_django.py
@@ -0,0 +1,2 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '07v&&+v@im(2=-@%!mm8$89yjm5^sreqxo+51&p=b9tn-%ew+!'
--- pokemon_project/settings.py
+++ pokemon_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from secrec_django import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -19,8 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '07v&&+v@im(2=-@%!mm8$89yjm5^sreqxo+51&p=b9tn-%ew+!'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
